773d3092ea0f4a65a49b77e83b0d7d51ef11f943
Kat B <katreesebarnes@gmail.com>
2019-03-31 17:55:29 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,35 @@
+require "pry"
+class Song 
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  attr_accessor :name, :artist, :genre
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+  def self.count 
+    @@count 
+  end
+  def self.genres 
+    @@genres.uniq 
+  end
+  def self.artists 
+    @@artists.uniq
+  end 
+  def self.genre_count
+    genre_count = {}
+    @@genres.each_with_index do |genre, index|
+    if  genre_count[genre] 
+      genre_count[genre] = index += 1 
+    else 
+      genre_count[genre] = 1 
+    end
+  end
+   genre_count
+  end
+end
