490c3179d58affb7b30e3b094ee821c5af699b7c
James Cox <jamesncox@yahoo.com>
2019-04-01 17:20:19 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,52 @@
+require 'pry'
+
+class Song
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0
+  # @@genres = []
+  # @@artist = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@artists << artist
+    @@genres << genre
+    @@count += 1
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  @@genres = []
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+  @@artists = []
+  
+  def self.artists
+    @@artists.uniq
+  end
+   
+  def self.genre_count
+    genre_count = Hash.new(0)
+    @@genres.each do |genre|
+      genre_count[genre] += 1
+    end
+    genre_count
+    # binding.pry
+  end
+  
+  def self.artist_count
+    artist_count = Hash.new(0)
+    @@artists.each do |artist|
+      artist_count[artist] += 1
+    end
+    artist_count
+  end
+  
+end
